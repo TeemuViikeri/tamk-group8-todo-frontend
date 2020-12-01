c9d1fd1994101cc7a2aa605b4f7dd24000547390
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-02 00:04:41 +0200

    Send boolean values to Subheader from state hmap.
    * state hash map keys include display and boolean values
      * boolean values change when toggleDisplay() is called
    * toggleDisplay() changes both values in hash map
    * boolean values are sent as a prop to Subheader

--- todo/src/components/todo/TodoContainer.js
+++ todo/src/components/todo/TodoContainer.js
@@ -7,21 +7,20 @@ class TodoContainer extends Component {
     super(props);
 
     const map = new Map();
-    map.set(1, "block")
-    map.set(2, "block")
+    map.set(1, ["block", false])
+    map.set(2, ["block", false])
 
     this.state = {
       todos: [],
-      show: map
+      display: map,
     };
   }
 
   componentDidUpdate(prevState) {
-    if (this.state.show !== prevState.show) {
+    if (this.state.display !== prevState.display) {
       this.getWrapperStyle("wrapper-1")
       this.getWrapperStyle("wrapper-2")
     }
-
   }
 
   getFlexContainerStyle = () => {
@@ -35,17 +34,19 @@ class TodoContainer extends Component {
   getWrapperStyle = (wrapperId) => {    
     const e = document.getElementById(wrapperId);
     const id = wrapperId.slice(-1, wrapperId.length); 
-    const d = this.state.show.get(Number(id));
+    const d = this.state.display.get(Number(id))[0];
     e.style.display = d;
   }
 
-  toggleShow = (subheaderId) => {
-    const m = new Map(this.state.show)
+  toggleDisplay = (subheaderId) => {
+    const m = new Map(this.state.display)
     const id = subheaderId.slice(-1, subheaderId.length);
-    const d = m.get(Number(id));
-    m.set(Number(id), d === "block" ? "none" : "block");
+    const d = m.get(Number(id))[0];
+    const b = m.get(Number(id))[1];
     
-    this.setState({ show: m })
+    m.set(Number(id), d === "block" && b === false ? ["none", !b] : ["block", !b]);
+
+    this.setState({ display: m })
   }
 
   render() {
@@ -55,7 +56,8 @@ class TodoContainer extends Component {
           <Subheader  
             id={"sub-1"}
             name="To Do"
-            toggleShow={this.toggleShow}
+            toggleDisplay={this.toggleDisplay}
+            up={this.state.display.get(1)[1]}
           />
           <div id="wrapper-1">
             {this.props.todos.map((todo) => {
@@ -73,9 +75,10 @@ class TodoContainer extends Component {
         </div>
         <div style={{ width: "50%" }}>
           <Subheader  
-            id="2"
+            id="sub-2"
             name="Done" 
-            toggleShow={this.toggleShow}
+            toggleDisplay={this.toggleDisplay}
+            up={this.state.display.get(2)[1]}
           />
           <div id="wrapper-2">
             {this.props.doneTodos.map((todo) => {
