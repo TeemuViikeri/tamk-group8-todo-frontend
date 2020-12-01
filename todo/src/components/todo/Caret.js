732cdfd2e9ec08f9037f57b9453d805cb09716c6
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-02 00:16:38 +0200

    Rotate Caret depending on "up" prop bool value.

--- todo/src/components/todo/Caret.js
+++ todo/src/components/todo/Caret.js
@@ -1,24 +1,35 @@
 import React, { Component } from "react";
 
 class Caret extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      up: this.props.up,
+    }
+  }
+
   getCaretStyle = () => {
     return {
+      display: "inline-block",
       width: "0",
       height: "0",
       borderStyle: "solid",
       borderWidth: "6px 6px 0 6px",
       borderColor: "#cc5252 transparent transparent transparent",
       position: "relative",
-      top: "21px",
-      left: "10px",
-      cursor: "pointer"
+      bottom: "6px",
+      left: "6px",
+      cursor: "pointer",
+      transform: this.props.up ? "rotate(0.5turn)" : "",
+      transition: "all .2s linear"
     }
   }
-
+  
   render() {
     return (
       <span 
-        onClick={this.props.toggleShow.bind(this, this.props.id)}
+        onClick={this.props.toggleDisplay.bind(this, this.props.id)}
         style={this.getCaretStyle()}
       >
       </span>
