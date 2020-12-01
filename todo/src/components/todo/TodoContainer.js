03e261f360c57ba17d83a4b4348f940c0e4c36f6
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-01 15:17:50 +0200

    Make displaying toggle work in TodoContainer.
    * Map in state which contains changing display values
    * toggleShow() toggles values in map  between "block" and "none"
    * toggleShow() is sent as a prop to Subheader
    * getWrapperStyle() sets correct display value for todo wrapper div
    * componentDidUpdate() calls getWrapperStyle() for both wrappers

--- todo/src/components/todo/TodoContainer.js
+++ todo/src/components/todo/TodoContainer.js
@@ -6,9 +6,22 @@ class TodoContainer extends Component {
   constructor(props) {
     super(props);
 
+    const map = new Map();
+    map.set(1, "block")
+    map.set(2, "block")
+
     this.state = {
       todos: [],
+      show: map
     };
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.show !== prevState.show) {
+      this.getWrapperStyle("wrapper-1")
+      this.getWrapperStyle("wrapper-2")
+    }
+
   }
 
   getFlexContainerStyle = () => {
@@ -19,36 +32,64 @@ class TodoContainer extends Component {
     }
   }
 
+  getWrapperStyle = (wrapperId) => {    
+    const e = document.getElementById(wrapperId);
+    const id = wrapperId.slice(-1, wrapperId.length); 
+    const d = this.state.show.get(Number(id));
+    e.style.display = d;
+  }
+
+  toggleShow = (subheaderId) => {
+    const m = new Map(this.state.show)
+    const id = subheaderId.slice(-1, subheaderId.length);
+    const d = m.get(Number(id));
+    m.set(Number(id), d === "block" ? "none" : "block");
+    
+    this.setState({ show: m })
+  }
+
   render() {
     return (
       <div style={this.getFlexContainerStyle()}>
         <div style={{ width: "50%" }}>
-          <Subheader name={"To Do"} />
-          {this.props.todos.map((todo) => {
-            return (
-              <TodoItem
-                key={todo.id}
-                todo={todo}
-                toggleTodo={this.props.toggleTodo}
-                deleteTodo={this.props.deleteTodo}
-                checked={todo.is_done}
-              />
-            );
-          })}
+          <Subheader  
+            id={"sub-1"}
+            name="To Do"
+            toggleShow={this.toggleShow}
+          />
+          <div id="wrapper-1">
+            {this.props.todos.map((todo) => {
+              return (
+                <TodoItem
+                  key={todo.id} 
+                  todo={todo}
+                  toggleTodo={this.props.toggleTodo}
+                  deleteTodo={this.props.deleteTodo}
+                  checked={todo.is_done}
+                />
+              );
+            })}
+          </div>
         </div>
         <div style={{ width: "50%" }}>
-          <Subheader name={"Done"} />
-          {this.props.doneTodos.map((todo) => {
-            return (
-              <TodoItem
-                key={todo.id}
-                todo={todo}
-                toggleTodo={this.props.toggleTodo}
-                deleteTodo={this.props.deleteTodo}
-                checked={todo.is_done}
-              />
-            );
-          })}
+          <Subheader  
+            id="2"
+            name="Done" 
+            toggleShow={this.toggleShow}
+          />
+          <div id="wrapper-2">
+            {this.props.doneTodos.map((todo) => {
+              return (
+                <TodoItem
+                  key={todo.id}
+                  todo={todo}
+                  toggleTodo={this.props.toggleTodo}
+                  deleteTodo={this.props.deleteTodo}
+                  checked={todo.is_done}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     );
