f1ea27c2572f2bf352fdaa909ade5753c427fef7
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-02 12:47:09 +0200

    Make TextInput more modular with props.

--- todo/src/components/todo/TextInput.js
+++ todo/src/components/todo/TextInput.js
@@ -3,9 +3,12 @@ import React, { Component } from "react";
 class TextInput extends Component {
   getTextInputStyle = () => {
     return {
-      flex: "10",
-      padding: "10px",
-      margin: "15px 20px",
+      display: "inline-block",
+      flex: this.props.flex,
+      width: "100%",
+      padding: this.props.padding,
+      margin: this.props.margin,
+      verticalAlign: this.props.verticalAlign,
       boxShadow: "0px -0px 1px 4px rgba(255, 255, 255, .2)",
       border: "none",
       borderRadius: "12px",
@@ -15,14 +18,14 @@ class TextInput extends Component {
 
   render() {
     return (
-      <input
-        type="text"
-        name="title"
-        style={this.getTextInputStyle()}
-        placeholder="Any homework to do?"
-        value={this.props.title}
-        onChange={this.props.onChange}
-      />
+        <input
+          type="text"
+          name="title"
+          style={this.getTextInputStyle()}
+          placeholder={this.props.placeholder}
+          value={this.props.title}
+          onChange={this.props.onChange}
+        />
     );
   }
 }
