bb111f2f9d6530e9da653853d28a37a2ce90139d
iskela45 <iskela45@gmail.com>
2020-12-02 13:12:06 +0200

    remove unnecessary functions

--- todo/src/components/todo/SortMenu.js
+++ todo/src/components/todo/SortMenu.js
@@ -10,45 +10,7 @@ class SortMenu extends React.Component {
     this.state = {
       showMenu: false,
     };
-
-    this.showMenu = this.showMenu.bind(this);
-    this.closeMenu = this.closeMenu.bind(this);
   }
-
-  showMenu(event) {
-    event.preventDefault();
-    
-    this.setState({ showMenu: true }, () => {
-      document.addEventListener('click', this.closeMenu);
-    });
-  }
-
-  closeMenu(event) {
-    
-    if (!this.dropdownMenu.contains(event.target)) {
-      
-      this.setState({ showMenu: false }, () => {
-        document.removeEventListener('click', this.closeMenu);
-      });  
-      
-    }
-  }
-
-  getSubmenuStyle = () => {
-    return {
-      position: "absolute",
-      backgroundColor: "#f9f9f9",
-      minWidth: "160px",
-      boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.2)",
-      zIndex: 1,
-    };
-  };
-
-  getMenuStyle = () => {
-    return {
-      float: "right",
-    };
-  };
 
   render() {
     return (
