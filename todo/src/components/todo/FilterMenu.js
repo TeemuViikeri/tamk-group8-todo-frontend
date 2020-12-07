7205a0ac04376537612a6a53748acd52c6097e72
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-07 14:42:39 +0200

    Remove useless styling functions from FilterMenu.

--- todo/src/components/todo/FilterMenu.js
+++ todo/src/components/todo/FilterMenu.js
@@ -34,22 +34,6 @@ class FilterMenu extends React.Component {
     }
   }
 
-  getSubmenuStyle = () => {
-    return {
-      position: "absolute",
-      backgroundColor: "#f9f9f9",
-      minWidth: "160px",
-      boxShadow: "0px 8px 16px 0px rgba(0,0,0,0.2)",
-      zIndex: 1,
-    };
-  };
-
-  getMenuStyle = () => {
-    return {
-      float: "right",
-    };
-  };
-
   render() {
     return (
       <div className="dropdown">
