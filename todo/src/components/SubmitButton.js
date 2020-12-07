168556e5e04e4bb7da9ece6e488052da32f7854c
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-08 00:20:15 +0200

    Remove box shadows, add flex from props.

--- todo/src/components/SubmitButton.js
+++ todo/src/components/SubmitButton.js
@@ -3,11 +3,10 @@ import React, { Component } from "react";
 class SubmitButton extends Component {
   getSubmitButtonStyle = () => {
     return {
-      flex: "1",
+      flex: this.props.flexBtn,
       backgroundColor: this.props.bgColorSubmit,
       color: this.props.textColorSubmit,
       margin: "15px 20px 15px 0",
-      boxShadow: "0px -0px 1px 4px rgba(255, 255, 255, .2)",
       border: "none",
       borderRadius: this.props.borderRadiusSubmit,
     };
