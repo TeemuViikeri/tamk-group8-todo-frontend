b731474bf2cf7795e7d1e9ff9cb96a97f59bcf87
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-07 23:57:02 +0200

    Change and add props, remove box shadows.

--- todo/src/components/todo/TextInput.js
+++ todo/src/components/todo/TextInput.js
@@ -3,13 +3,11 @@ import React, { Component } from "react";
 class TextInput extends Component {
   getTextInputStyle = () => {
     return {
-      width: this.props.width,
       display: "inline-block",
-      flex: this.props.flex,
+      flex: this.props.flexInput,
       padding: this.props.padding,
       margin: this.props.margin,
       verticalAlign: this.props.verticalAlign,
-      boxShadow: "0px -0px 1px 4px rgba(255, 255, 255, .2)",
       border: "none",
       borderBottom: this.props.borderBottom,
       borderRadius: this.props.borderRadiusInput,
@@ -20,16 +18,16 @@ class TextInput extends Component {
 
   render() {
     return (
-        <input
-          type="text"
-          name="title"
-          style={this.getTextInputStyle()}
-          placeholder={this.props.placeholder}
-          value={this.props.title}
-          onChange={this.props.onChange}
-          borderBottom={this.props.borderBottom}
-          borderRadius={this.props.borderRadiusInput}
-        />
+      <input
+        type="text"
+        name="title"
+        style={this.getTextInputStyle()}
+        placeholder={this.props.placeholder}
+        value={this.props.title}
+        onChange={this.props.onChange}
+        borderBottom={this.props.borderBottom}
+        borderRadius={this.props.borderRadiusInput}
+      />
     );
   }
 }
