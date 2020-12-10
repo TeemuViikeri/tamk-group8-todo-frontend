b93d845a50bfb3f929e17425662067382f5b769b
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-11 00:10:30 +0200

    Add color buttons to dropdown content container.

--- todo/src/components/todo/ColorMenu.js
+++ todo/src/components/todo/ColorMenu.js
@@ -7,6 +7,7 @@ class FilterMenu extends React.Component {
 
   constructor(props) {
     super(props)
+    
     this.state = {
       showMenu: false,
     };
@@ -39,11 +40,71 @@ class FilterMenu extends React.Component {
       <div className="dropdown colorMenu">
         <button className="dropbtn"><FontAwesomeIcon icon={faPalette} size="2x" /></button>
         <div className="dropdown-content">
-          {
-          /* <button onClick={this.props.setDeadlineFilter.bind(this, "")}>Show all</button>
-          <button onClick={this.props.setDeadlineFilter.bind(this, "notNull")}>Show tasks with deadlines</button>
-          <button onClick={this.props.setDeadlineFilter.bind(this, "null")}>Show tasks without deadlines</button> */
-          }
+          <button 
+            onClick={this.props.setColor.bind(this, this.props.currentList, 0 )}
+            style={{ 
+              display: "inline-block",
+              backgroundColor: "#cc5252",
+              width: "24px",
+              height: "24px",
+              margin: "4px",
+              border: "none",
+              borderRadius: "50%",
+              cursor: "pointer"
+            }} 
+            />
+          <button 
+            onClick={this.props.setColor.bind(this, this.props.currentList, 1 )}
+            style={{ 
+              display: "inline-block",
+              backgroundColor: "blue",
+              width: "24px",
+              height: "24px",
+              margin: "4px",
+              border: "none",
+              borderRadius: "50%",
+              cursor: "pointer"
+            }} 
+          />
+          <button 
+            onClick={this.props.setColor.bind(this, this.props.currentList, 2 )}
+            style={{ 
+              display: "inline-block",
+              backgroundColor: "green",
+              width: "24px",
+              height: "24px",
+              margin: "4px",
+              border: "none",
+              borderRadius: "50%",
+              cursor: "pointer"
+            }} 
+          />
+          <button 
+            onClick={this.props.setColor.bind(this, this.props.currentList, 3 )}
+            style={{ 
+              display: "inline-block",
+              backgroundColor: "darkviolet",
+              width: "24px",
+              height: "24px",
+              margin: "4px",
+              border: "none",
+              borderRadius: "50%",
+              cursor: "pointer"
+            }} 
+          />
+          <button 
+            onClick={this.props.setColor.bind(this, this.props.currentList, 4 )}
+            style={{ 
+              display: "inline-block",
+              backgroundColor: "#333",
+              width: "24px",
+              height: "24px",
+              margin: "4px",
+              border: "none",
+              borderRadius: "50%",
+              cursor: "pointer"
+            }} 
+          />
         </div>
     </div> 
     )
