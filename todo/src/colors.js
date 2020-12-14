1e85a6a5cd4ad7a80b103bc345488201c77c3e21
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-14 11:27:55 +0200

    Create separate color property file for themes.
    File includes red, blue, green, violet, dark.

---
+++ todo/src/colors.js
@@ -0,0 +1,89 @@
+var colors = {
+  // RED: default
+  red: {
+    primary: "#cc5252",
+    secondary: "#962029",
+    bgPrimary: "white",
+    bgSecondary: "gainsboro",
+    fillList: "white",
+    fillActiveList: "#cc5252",
+    fillTask: "#f8f8f8",
+    fillEditBg: "#cc5252",
+    fillEditText: "white",
+    fillButton: "gainsboro",
+    fillHeaderColor: "white",
+    fillTextColor: "black",
+    fillActiveTextColor: "white",
+    fillInputColor: "white"
+  },
+  // BLUE
+  blue: {
+    primary: "#2554C7",
+    secondary: "#193a8b",
+    bgPrimary: "white",
+    bgSecondary: "gainsboro",
+    fillList: "white",
+    fillActiveList: "#2554C7",
+    fillTask: "#f8f8f8",
+    fillEditBg: "#2554C7",
+    fillEditText: "white",
+    fillButton: "gainsboro",
+    fillHeaderColor: "white",
+    fillTextColor: "black",
+    fillActiveTextColor: "white",
+    fillInputColor: "white"
+  },
+  // GREEN
+  green: {
+    primary: "#278427",
+    secondary: "#1a581a",
+    bgPrimary: "white",
+    bgSecondary: "gainsboro",
+    fillList: "white",
+    fillActiveList: "#278427",
+    fillTask: "#f8f8f8",
+    fillEditBg: "#278427",
+    fillEditText: "white",
+    fillButton: "gainsboro",
+    fillHeaderColor: "white",
+    fillTextColor: "black",
+    fillActiveTextColor: "white",
+    fillInputColor: "white"
+  },
+  // VIOLET
+  violet: {
+    primary: "#6e2ac7",
+    secondary: "#461B7E",
+    bgPrimary: "white",
+    bgSecondary: "gainsboro",
+    fillList: "white",
+    fillActiveList: "#6e2ac7", 
+    fillTask: "#f8f8f8",
+    fillEditBg: "#6e2ac7",
+    fillEditText: "white",
+    fillButton: "gainsboro",
+    fillHeaderColor: "white",
+    fillTextColor: "black",
+    fillActiveTextColor: "white",
+    fillInputColor: "white"
+  },
+  // DARK
+  dark: {
+    primary: "#333",
+    secondary: "#3a3a3a",
+    bgPrimary: "#1E1E1E",
+    bgSecondary: "#232323",
+    fillList: "#1e2021",
+    fillActiveList: "#444",
+    fillTask: "#444",
+    fillEditBg: "#444",
+    fillEditText: "whitesmoke",
+    fillButton: "#444",
+    fillHeaderColor: "whitesmoke",
+    fillTextColor: "whitesmoke",
+    fillActiveTextColor: "white",
+    fillInputColor: "#1e2021"
+  },
+}
+
+export default colors;
