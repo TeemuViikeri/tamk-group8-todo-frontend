c86bb64efcaf97f3b05588370f5338f7fc42b6a6
TeemuViikeri <teemu.viikeri@tuni.fi>
2020-12-17 23:01:07 +0200

    Add scrolling dropdown menu for searching tasks.

--- todo/src/components/todo/SearchField.js
+++ todo/src/components/todo/SearchField.js
@@ -1,20 +1,43 @@
 import React, { Component } from "react";
 import TextInputField from "./TextInputField";
+import "./css/dropdown.css";
 
 class SearchField extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      title: ""
+      title: "",
+      isSearching: false,
     };
   }
 
   getStyle = () => {
     return {
       flex: "1",
-      display: "inline-block"
+      display: "inline-block",
+      maxWidth: "100%",
+      overflow: "auto"
     }
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if (this.props.searchTasks !== prevProps.searchTasks) {
+      this.setState({ isSearching: true })
+    }
+  }
+
+  getListName = (lists, todo) => {
+    const { list_id } = todo;
+    let name = "";
+
+    for (const list of lists) {
+      if (list.id === list_id) {
+        name = list.name
+      }
+    }
+
+    return name
   }
 
   render() {
@@ -24,22 +47,65 @@ class SearchField extends Component {
           idInput="search"
           title={this.state.title} 
           handleOnKeyUp={this.props.handleOnKeyUp}
-          placeholder="Search task"
+          placeholder="Search tasks"
           paddingInput="0.625rem"
-          paddingSubmit="0.625rem"
-          marginInput="0.9375rem 0 0.9375rem 1.25rem"
-          marginSubmit="0.9375rem 1rem 0.9375rem 0"
+          marginInput="0.9375rem 0.9375rem 0.9375rem 1.25rem"
           flexInput="3"
-          flexBtn="1"
-          btnText="Find"
           borderBottom="none"
-          borderRadiusInput="0.75rem 0 0 0.75rem"
-          borderRadiusSubmit="0 0.75rem 0.75rem 0"
+          borderRadiusInput="0.75rem 0.75rem 0.75rem 0.75rem"
           bgColorInput={this.props.palette.fillInputColor}
           textColorInput={this.props.palette.fillTextColor}
-          bgColorSubmit={this.props.palette.fillButton}
-          textColorSubmit={this.props.palette.fillTextColor}
-        />
+          showButton={false}
+          isSearching={this.state.isSearching}
+          autocomplete="off"
+          />
+        {
+          (this.state.isSearching)
+          ? <div 
+              className="search-container"
+              style={{ 
+                color: this.props.palette.fillTextColor, 
+                width: "224px",
+                height: "50%",
+                backgroundColor: this.props.palette.bgPrimary,
+                position: "absolute",
+                top: "70px",
+                right: "8%",
+                zIndex: 1,
+                overflowY: "auto",
+                overflowX: "hidden",
+                border: `2px solid ${this.props.palette.primary}`,
+                borderRadius: "4px"
+              }}
+            >
+              <h3 style={{ color: this.props.palette.primary, fontWeight: "bold", margin: "0.5rem 0 0 1rem", fontSize: "1.75rem" }}>
+                Tasks
+                <span 
+                  onClick={() => this.setState({ isSearching: false })} 
+                  style={{ 
+                    color: this.props.palette.fillClearButton, 
+                    float: "right", 
+                    marginRight: "1rem", 
+                    cursor: "pointer" 
+                  }}
+                >
+                  &times;
+                </span>
+              </h3>
+              <ul style={{ listStyleType: "none"}}>
+                {this.props.searchTasks.map((task, i) => 
+                  <li key={i} style={{ margin: "1rem" }}>
+                    <p style={{ fontWeight: "600" }}>{task.title}</p>
+                    <p style={{ fontStyle: "italic", fontSize: "0.8rem"}}>
+                      {
+                        this.getListName(this.props.lists, task)
+                      }
+                    </p>
+                  </li>)}
+              </ul>
+            </div>
+          : <></>
+        }
       </div>
     )
   }
